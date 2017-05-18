a593d5ea26fb486b1af61aa7de1c97c7ac7c726c
vladyn <vlado@vlado1.com>
2017-05-18 11:25:29 +0200

    simple module loading try

---
+++ js/modules/logger-compiled.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = foo;
+function foo(msg) {
+    if (msg != undefined) {
+        console.info(msg);
+    }
+}
+
+//# sourceMappingURL=logger-compiled.js.map
